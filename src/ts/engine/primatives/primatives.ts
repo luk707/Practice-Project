d1641845020c69b3bdf37ec5ee0f915852abe70c
Luke Harris <lukeharris01170@gmail.com>
2016-11-23 22:57:00 +0000

    Added background primative

--- src/ts/engine/primatives/primatives.ts
+++ src/ts/engine/primatives/primatives.ts
@@ -1,3 +1,4 @@
 import {Axes} from "./axes"; export {Axes};
 import {Box} from "./box"; export {Box};
 import {Grid} from "./grid"; export {Grid};
+import {Background} from "./background"; export {Background};
