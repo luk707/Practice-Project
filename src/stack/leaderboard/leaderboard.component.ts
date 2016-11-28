023452dc34132b9811f5d49a55f2911e829c1714
Luke Harris <lukeharris01170@gmail.com>
2016-11-28 14:26:50 +0000

    Added leaderboard page (#18)

---
+++ src/stack/leaderboard/leaderboard.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'leaderboard',
+    template: require('./leaderboard.component.html')
+})
+
+export class LeaderboardComponent {
+}
--- src/stack/stack.routing.ts
+++ src/stack/stack.routing.ts
@@ -3,10 +3,12 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
 import {GameComponent} from './game/game.component';
+import {LeaderboardComponent} from './leaderboard/leaderboard.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'play', component: GameComponent }
+    { path: 'play', component: GameComponent },
+    { path: 'leaderboard', component: LeaderboardComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
