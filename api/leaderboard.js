2be7953dc96dc09f17787ac68b26f5a3f0a6ff90
Luke Harris <lukeharris01170@gmail.com>
2016-12-15 15:49:26 +0000

    Dummy JSON files

---
+++ api/leaderboard.js
@@ -0,0 +1,25 @@
+// TODO: Produce ordered list of top 5 scores using a database
+module.exports = function() {
+    return JSON.stringify([
+        {
+            name: "bill",
+            score: 15
+        },
+        {
+            name: "bob",
+            score: 14
+        },
+        {
+            name: "ben",
+            score: 13
+        },
+        {
+            name: "bobby",
+            score: 12
+        },
+        {
+            name: "bourne",
+            score: 11
+        }
+    ]);
+};
--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+var leaderboard = require('./api/leaderboard');
+
+app.use('/api/:service', function(req, res, next) {
+    switch(req.params.service) {
+        default:
+            next();
+            break;
+        case "leaderboard":
+            res.send(leaderboard());
+    }
+});
 
 app.use(express.static('dist'));
 
-app.listen(80);
+app.get('*', function(req, res) {
+    res.redirect('/');
+});
+
+app.listen(80, function() {
+    console.log("----STACK----");
+    console.log("Server started on port: 80");
+    console.log("Ctrl + C to terminate");
+});
