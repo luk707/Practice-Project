3dac511f38835595e671aa22fd287602171c61a0
boz83 <steven_stamford@yahoo.co.uk>
2016-12-16 02:11:38 +0000

    Added Background, Fixed contrasting and Added content
    Added the WebGL fade background to the web page, changed the title and
    other text elements to improve their contrast and readability as well as
    adding visual aids to assist the user

--- src/stack/help/help.component.ts
+++ src/stack/help/help.component.ts
@@ -1,9 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, AfterViewInit} from '@angular/core';
+import {GameViewComponent} from '../game-view/game-view.component';
+import * as Engine from "../../ts/engine/engine";
 
 @Component({
     selector: 'help',
     template: require('./help.component.html')
 })
 
-export class HelpComponent {
+export class HelpComponent implements AfterViewInit {
+    private view: Engine.GameView;
+    private basicProgram: WebGLProgram;
+    private camera: Engine.Camera;
+    private background: Engine.Primatives.Background;
+    private Start(){
+        this.basicProgram = this.view.CreateBasicProgram();
+        this.camera = new Engine.Camera();
+        this.background = new Engine.Primatives.Background(this.view);
+        this.background.color1 = new Engine.Color(0.55, 0.72, 0.66);
+        this.background.color2 = new Engine.Color(0.1, 0.55, 0.62);
+    }
+    private Render() {
+        let viewMatrix = this.camera.View();
+        let projectionMatrix = this.camera.Projecttion();
+        this.view.gl.clear(this.view.gl.COLOR_BUFFER_BIT | this.view.gl.DEPTH_BUFFER_BIT);
+        this.view.DrawObject(this.background, this.basicProgram, viewMatrix, projectionMatrix);
+    }
+
+
+    @ViewChild(GameViewComponent) gameViewComponent: GameViewComponent;
+
+    ngAfterViewInit(){
+        this.gameViewComponent.OnInit((view: Engine.GameView)=>{
+            this.view = view;
+            this.Start();
+            this.Render();
+        });
+    }
 }
